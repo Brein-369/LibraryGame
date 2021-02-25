b01b9eceea1909e755eb6e3919c1e854420bbcf4
Brein-369 <edwardbrainerd@gmail.com>
2021-02-25 17:09:07 +0700

    delete node modules

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,18 @@
 const {Game, User, UserGame} = require('../models') 
+const user = require('../models/user')
+const helper = require('../helpers')
 
 class Controller {
 
-    static 
+    static showAllUsers(req, res){
+        User.findAll()
+        .then((data)=>{
+            res.render
+        })
+        .catch(err=>{
+            res.send(helper)
+        })
+    }
 
 }
 
